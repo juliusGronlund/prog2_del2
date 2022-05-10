e01995d6927d7f32440dc1dccc70161096c8ffa6
juliusGronlund <juliusgroenlund@gmail.com>
2022-05-10 11:45:58 +0200

    dag 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-
     }
 }
