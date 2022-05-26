ff2213b47f962713d3ab0e24aee17ab3cf525c22
juliusGronlund <juliusgroenlund@gmail.com>
2022-05-26 11:51:36 +0200

    Changes by gustavwalter

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,29 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.scene.layout.Pane;
+
+import java.io.*;
+import java.util.*;
 
 public class Main extends Application {
     // Global variables available to all classes
     private Stage stage;
     private BorderPane root;
+    private Pane pane;
     private BorderPane top;
+    private ImageView imageView;
+    private ListGraph<City> listGraph;
 
     @Override
     public void start(Stage stage) throws Exception {
+        listGraph = new ListGraph();
+        pane = new Pane();
+
         // Move stage to global variable
         this.stage = stage;
 
@@ -53,19 +65,7 @@ public class Main extends Application {
     class NewMapHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent actionEvent) {
-            double rootHeight = root.getHeight();
-            //double topHeight = top.getTop().maxHeight();
-            Image map = new Image("file:/Users/Julius/Documents/IntelliJProjects/prog2_del2/src/europa.gif");
-            ImageView imageView = new ImageView(map);
-            root.setCenter(imageView);
-
-            // Change window size so map fits
-            // Detta måste vi fixa så att det inte är hårdkodat
-            double mapWidth = map.getWidth();
-            double mapHeight = map.getHeight() + rootHeight + 30;
-
-            stage.setWidth(mapWidth);
-            stage.setHeight(mapHeight);
+            imageView = createMap();
         }
     }
 
@@ -73,137 +73,114 @@ public class Main extends Application {
     class OpenHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent actionEvent) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "OpenHandler");
-            alert.showAndWait();
-        }
+            try{
+                File file = new File("/Users/Julius/Documents/IntelliJProjects/prog2_del2/src/europa.graph");
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
 
+                imageView = createMap();
+                pane.getChildren().add(imageView);
+                root.setCenter(pane);
 
+                String data;
+                String[] nodeInfo = null;
+                int lineCounter = 0;
 
-        private void readCityLine(String data) {
-            int counter = 0;
-            String cityName = "";
-            String tempString = "";
-            double xCordinate = 0;
-            double yCordinate = 0;
-            StringBuilder cityStringBuilder = new StringBuilder();
-            StringBuilder cordinateStringBuilder = new StringBuilder();
+                //Loop som kör så länge det finns data kvar att läsa från filen
+                while ((data = br.readLine()) != null){
+                    nodeInfo = data.split(";");
 
-            for (int i = 0; i <= data.length() + 1; i++) {
-                if(counter == 3){
-                    listGraph.add(new City(cityName, xCordinate, yCordinate));
-
-                    cityName = "";
-                    xCordinate = 0;
-                    yCordinate = 0;
-                    counter = 0;
-                }
-
-                char character;
-
-                if(i < data.length()) {
-                    character = data.charAt(i);
-                }
-                //Detta för kompensera att det inte finns ett ; i slutet av raden.
-                else {
-                    character = ';';
-                }
-
-
-                if(character == ';'){
-                    if(counter == 0){
-                        cityName = cityStringBuilder.toString();
-                        cityStringBuilder.setLength(0);
+                    if(lineCounter == 0){
+                        // Här säger vi vilken fil
                     }
 
-                    tempString = cordinateStringBuilder.toString();
-                    if(counter == 1){
-                        xCordinate = Double.parseDouble(tempString);
+                    if(lineCounter == 1){
+                        // Här skapar vi städerna
+                        for(int i = 0; i < nodeInfo.length; i = i+3){
+                            String stad = nodeInfo[i];
+                            double xCordinate = Double.parseDouble(nodeInfo[i+1]);
+                            double yCordinate = Double.parseDouble(nodeInfo[i+2]);
+
+                            // Skapa staden
+                            City city = new City(stad, xCordinate, yCordinate);
+                            listGraph.add(city);
+
+                            // Skapa cirkel
+                            Circle circleForCity = new Circle();
+                            circleForCity.setCenterX(xCordinate);
+                            circleForCity.setCenterY(yCordinate);
+                            circleForCity.setRadius(10.0f);
+                            circleForCity.setFill(Color.BLUE);
+
+                            // Skapa label
+                            Label label = new Label(stad);
+                            label.setLayoutX(xCordinate - 10);
+                            label.setLayoutY(yCordinate + 10);
+
+                            // Lägg till label och cirkel i pane
+                            pane.getChildren().add(label);
+                            pane.getChildren().add(circleForCity);
+                        }
                     }
 
-                    if(counter == 2){
-                        yCordinate = Double.parseDouble(tempString);
-                    }
-                    cordinateStringBuilder.setLength(0);
-                    tempString = "";
+                    if(lineCounter > 1){
+                        // Här skapar vi edges
+                        for(int i = 0; i < nodeInfo.length; i=i+4){
+                            String city1Name = nodeInfo[i];
+                            String city2Name = nodeInfo[i+1];
+                            String edgeName = nodeInfo[i+2];
+                            int weight = Integer.parseInt(nodeInfo[i+3]);
 
-                    counter++;
-                } else {
-                    switch (counter){
-                        case 0:
-                            cityStringBuilder.append(cityName).append(character);
-                            break;
-                        case 1:
-                            cordinateStringBuilder.append(tempString).append(character);
-                            break;
-                        case 2:
-                            cordinateStringBuilder.append(tempString).append(character);
-                            break;
+                            City city1 = null;
+                            City city2 = null;
+
+                            Set set = listGraph.getNodes();
+
+                            Iterator<City> itr = set.iterator();
+
+                            while (itr.hasNext()) {
+
+                                //itr.next() får bara förekomma en gång per iteration.
+                                City temp = itr.next();
+
+                                if(temp.getName().equals(city1Name)) {
+                                    city1 = temp;
+                                    System.out.println("1 " + city1.getName());
+                                }
+                                if(temp.getName().equals(city2Name)){
+                                    city2 = temp;
+                                    System.out.println("2 " + city2.getName());
+                                }
+                                if(city1 != null && city2 != null) {
+                                    System.out.println("city1 != null && city2 != null");
+                                    listGraph.connect(city1, city2, edgeName, weight);
+                                }
+                            }
+
+                            /*
+                            Set<City> set = listGraph.getNodes();
+                            for(City temp : set){
+                                if(temp.getName().equals(city1Name)){
+                                    city1 = temp;
+                                }
+                                if(temp.getName().equals(city2Name)){
+                                    city2 = temp;
+                                }
+                            }
+                             */
+                        }
                     }
+                    lineCounter++;
                 }
+
+
+
+            } catch (FileNotFoundException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "file not found, " + e);
+                alert.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-
-        private void readEdgeLine(String data){
-            String destinationCity = "";
-            String departureCity = "";
-            String transportName = "";
-            int weight = 0;
-            int counter = 0;
-            StringBuilder stringBuilder = new StringBuilder();
-
-            for (int i = 0; i <= data.length() + 1; i++) {
-                char character;
-
-                if(i < data.length()) {
-                    character = data.charAt(i);
-                }
-                //Detta för kompensera att det inte finns ett ; i slutet av raden.
-                else {
-                    character = ';';
-                }
-
-                if(character == ';'){
-                    switch(counter){
-                        case 0:
-                            departureCity = stringBuilder.toString();
-                            stringBuilder.setLength(0);
-                            break;
-                        case 1:
-                            destinationCity = stringBuilder.toString();
-                            stringBuilder.setLength(0);
-                            break;
-                        case 2:
-                            transportName = stringBuilder.toString();
-                            stringBuilder.setLength(0);
-                            break;
-                        case 3:
-                            weight = Integer.parseInt(stringBuilder.toString());
-                            stringBuilder.setLength(0);
-                            break;
-                    }
-                    counter++;
-                } else{
-                    switch(counter){
-                        case 0:
-                            stringBuilder.append(departureCity).append(character);
-                            break;
-                        case 1:
-                            stringBuilder.append(destinationCity).append(character);
-                            break;
-                        case 2:
-                            stringBuilder.append(transportName).append(character);
-                            break;
-                        case 3:
-                            stringBuilder.append(weight).append(character);
-                            break;
-                    }
-                }
-            }
-            System.out.println(departureCity + ", " + destinationCity + ", " + transportName + ", " + weight);
-        }
-
-        private void readImageLine(String data) {
-            System.out.println(data);
         }
     }
 
@@ -273,6 +250,23 @@ public class Main extends Application {
     ///////////////////////////////////////
     //////////// Help methods /////////////
     ///////////////////////////////////////
+    private ImageView createMap(){
+        double rootHeight = root.getHeight();
+        Image map = new Image("file:/Users/Julius/Documents/IntelliJProjects/prog2_del2/src/europa.gif");
+        imageView = new ImageView(map);
+        root.setCenter(imageView);
+
+        // Change window size so map fits
+        // Detta måste vi fixa så att det inte är hårdkodat
+        double mapWidth = map.getWidth();
+        double mapHeight = map.getHeight() + rootHeight + 30;
+
+        stage.setWidth(mapWidth);
+        stage.setHeight(mapHeight);
+
+        return imageView;
+    }
+
     private VBox createMenu(){
         VBox vboxMenu = new VBox();
         MenuBar menuBar = new MenuBar();
